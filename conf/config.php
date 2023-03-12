b937982c3e3d1f012ef52e9189f9347d92011023
forest-chan <forest-chan@mail.ru>
2023-03-12 11:23:23 +0300

    added new button for toolbar

---
+++ conf/config.php
@@ -0,0 +1,10 @@
+<?php
+
+const PLUGIN_DIRECTORY = '/lib/plugins/xlsx2dw';
+
+return [
+    'buttonType' => 'format',
+    'buttonTitle' => 'Import Table',
+    'buttonIcon' => PLUGIN_DIRECTORY . '/images/bold.png',
+    'buttonBlock' => false,
+];
