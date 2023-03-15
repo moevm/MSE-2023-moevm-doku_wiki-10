9a6094b275e3040a96ef4f01aabd7afd4eb54825
ZhenjaMax <zhenja6@mail.ru>
2023-03-15 14:04:28 +0300

    Связь кнопки и скрипта, картинка
    - картинка для кнопки (/images/xlsx2dw-btn-icon.png);
    - добавлен id кнопки (xlsx2dw_btn);
    - добавлена связь кнопки и скрипта при помощи jQuery (script.js).

--- action.php
+++ action.php
@@ -1,30 +1,42 @@
 <?php
+
+if(!defined('DOKU_INC')) die();
+
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+require_once(DOKU_PLUGIN.'action.php');
+
 /**
  * DokuWiki Plugin xlsx2dw (Action Component)
  *
  * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
  * @author  moevm <ydginster@gmail.com>
  */
-class action_plugin_xlsx2dw extends \dokuwiki\Extension\ActionPlugin
+class action_plugin_xlsx2dw extends DokuWiki_Action_Plugin 
 {
 
     /** @inheritDoc */
     public function register(Doku_Event_Handler $controller)
     {
-        $controller->register_hook('TOOLBAR_DEFINE', 'FIXME', $this, 'handle_toolbar_define');
-   
+        $controller->register_hook('TOOLBAR_DEFINE', 'AFTER', $this, 'insert_button', array());
     }
 
     /**
-     * FIXME Event handler for
-     *
      * @param Doku_Event $event  event object by reference
      * @param mixed      $param  optional parameter passed when event was registered
      * @return void
      */
-    public function handle_toolbar_define(Doku_Event $event, $param)
-    {
-    }
+    public function insert_button(Doku_Event $event, $param) {
+        $config = require(DOKU_PLUGIN . 'xlsx2dw/conf/config.php');
 
+        $event->data[] = [
+            'type'   => $config['buttonType'],
+            'title'  => $config['buttonTitle'],
+            'icon'   => $config['buttonIcon'],
+            'id'     => $config['buttonID'],
+            'block'  => $config['buttonBlock'],
+            'open'   => $config['buttonOpen'],
+            'close'  => $config['buttonClose']
+        ];
+    }
 }
 
---
+++ conf/config.php
@@ -0,0 +1,13 @@
+<?php
+
+const PLUGIN_DIRECTORY = '/lib/plugins/xlsx2dw';
+
+return [
+    'buttonType' => 'format',
+    'buttonTitle' => '',
+    'buttonID'   => 'xlsx2dw_btn',
+    'buttonIcon' => PLUGIN_DIRECTORY . '/images/xlsx2dw-btn-icon.png',
+    'buttonBlock' => false,
+    'buttonOpen' => '',
+    'buttonClose' => ''
+];
